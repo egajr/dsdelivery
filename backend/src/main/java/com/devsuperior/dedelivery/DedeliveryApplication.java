bcb0f046d04ec347d8108e4d0b93b48a2e2f3649
Eder Geraldo Angelo Junior <edergajr@gmail.com>
2021-01-06 17:30:23 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dedelivery/DedeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dedelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DedeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DedeliveryApplication.class, args);
+	}
+
+}
